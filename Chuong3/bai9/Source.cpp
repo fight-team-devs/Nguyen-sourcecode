0c2189f2702b30d8962a6a9de400713bde4e8bd5
nguyen11097 <44333689+nguyen11097@users.noreply.github.com>
2019-11-08 00:49:14 +0700

    chuong3

--- Chuong3/bai9/Source.cpp
+++ Chuong3/bai9/Source.cpp
@@ -15,9 +15,7 @@ int main()
 		cout << "nhap nam: ";
 		cin >> nam;
 		x = nam % 4;
-		if (x == 0)
-		{
-			cout << "nam ban nha la nam nhuan"<<endl;
+		
 			switch (thang)
 			{
 			case 1: cout << "thang 1 nam " << nam << " co 31 ngay" << endl;break;
@@ -31,28 +29,19 @@ int main()
 			case 6: cout << "thang 6 nam " << nam << " co 30 ngay" << endl;break;
 			case 9: cout << "thang 9 nam " << nam << " co 30 ngay" << endl;break;
 			case 11: cout << "thang 11 nam " << nam << " co 30 ngay" << endl;break;
-			case 2: cout << "thang 2 nam " << nam << " co 29 ngay" << endl;break;
+			case 2:
+				if (x != 0)
+				{
+					cout << "thang 2 nam " << nam << " co 28 ngay" << endl;break;
+				}
+				else
+				{
+					cout << "thang 2 nam " << nam << " co 29 ngay" << endl;break;
+				}
 			}
-		}
-		else
-		{
-			cout << "nam ban nhap khong phai nam nhuan"<<endl;
-			switch (thang)
-			{
-			case 1: cout << "thang 1 nam " << nam << " co 31 ngay" << endl;break;
-			case 3: cout << "thang 3 nam " << nam << " co 31 ngay" << endl;break;
-			case 5: cout << "thang 5 nam " << nam << " co 31 ngay" << endl;break;
-			case 7: cout << "thang 7 nam " << nam << " co 31 ngay" << endl;break;
-			case 8: cout << "thang 8 nam " << nam << " co 31 ngay" << endl;break;
-			case 10: cout << "thang 10 nam " << nam << " co 31 ngay" << endl;break;
-			case 12: cout << "thang 12 nam " << nam << " co 31 ngay" << endl;break;
-			case 4: cout << "thang 4 nam " << nam << " co 30 ngay" << endl;break;
-			case 6: cout << "thang 6 nam " << nam << " co 30 ngay" << endl;break;
-			case 9: cout << "thang 9 nam " << nam << " co 30 ngay" << endl;break;
-			case 11: cout << "thang 11 nam " << nam << " co 30 ngay" << endl;break;
-			case 2: cout << "thang 2 nam " << nam << " co 28 ngay" << endl;break;
-			}
-		}
+		
+		
+		
 	}
 		system("pause");
 		return 0;
